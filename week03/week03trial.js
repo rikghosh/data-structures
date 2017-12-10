978862e2b10e04d3dc931ba6af05a667891d206b
rikghosh <riklimeghosh@gmail.com>
2017-12-10 18:31:32 +0000

    delete unnecessary files

---
+++ week03/week03trial.js
@@ -0,0 +1,33 @@
+ // npm install cheerio
+
+var fs = require('fs');
+var cheerio = require('cheerio');
+
+var addresses = [];
+
+// load the thesis text file into a variable, `content`
+var content = fs.readFileSync('data/m10.txt');
+
+// load `content` into a cheerio object
+var $ = cheerio.load(content);
+
+// introduce addresses
+console.log("The addresses are: \n");
+
+// navigate to the relevant table and loop through each row
+$('table table table tbody').find('tr').each(function(i, elem) {
+     // get the html code from the relevant element of each row, exctract the address line, fix addresses with extra information, and trim whitespace
+     // note: this page only had problems involving the two zipcodes below, but the replace method can be added for every new york zip code to ensure this works for other pages as well
+     addresses.push($(elem).find('td').first().html().split('<br>')[2].replace('NY 10032','').replace('NY 10033','').trim());
+});
+
+console.log(addresses);
+
+for (var i=0; i<addresses.length; i++) {
+    addresses[i] = addresses[i].split(',')[0];
+    addresses[i] = addresses[i] + ', New York, NY';
+}
+
+console.log(addresses);
+
+
