deaa5a81d828e19510cbd8a7b8931f174ce43f00
rikghosh <riklimeghosh@gmail.com>
2017-12-10 23:39:40 +0000

    adjust port

--- final_aa/app.js
+++ final_aa/app.js
@@ -117,7 +117,7 @@ app.get('/aa', function(req, res) {
     
 });
 
-app.listen(process.env.PORT, function() {
-// app.listen(4000, function() {
+// app.listen(process.env.PORT, function() {
+app.listen(4000, function() {
     console.log('Server listening...');
 });
